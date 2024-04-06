ad4a553cc959aea409961c6c3cb339c1b6e72872
nimashaliyanage <150325936+nimashaliyanage@users.noreply.github.com>
2024-04-06 17:26:55 +0530

    Create Footer.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import NavigationBar from "./Components/NavigationBar";
 import Home from "./Components/Home";
 import Body from "./Components/Body";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <NavigationBar />
       <Home />
       <Body />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,65 @@
+import { Container } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import "./Footer.css";
+import logo from "../Assets/ATD logo.png";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <Container>
+        <br />
+        <Row className="footer-row">
+          <Col md={12} lg={6}>
+            <img src={logo} alt="logo" className="footer-logo" />
+            <br />
+            <p className="textarea">
+              Your goal is our target. Not anything in between. We use online
+              marketing platforms and tools to achieve single objective - your
+              business results.
+            </p>
+            <br />
+            <br />
+          </Col>
+          <Col lg={3} className="foot-up-right">
+            <h3>Our Technologies</h3>
+            <h6>
+              <span>ReactJS</span>
+              <br />
+              <br />
+              <span>Gatsby</span>
+              <br />
+              <br />
+              <span>NextJS</span>
+              <br />
+              <br />
+              <span>NodeJS</span>
+            </h6>
+          </Col>
+          <br />
+          <br />
+          <Col lg={3} className="foot-up-right">
+            <h3>Our Services</h3>
+            <h6>
+              <span>Social Media Marketing</span>
+              <br />
+              <br />
+              <span>Web & Mobile App Development</span>
+              <br />
+              <br />
+              <span>Data & Analytics</span>
+            </h6>
+          </Col>
+          <br />
+          <br />
+        </Row>
+        <hr />
+        <Row className="copyrights">
+          <Col xs={12}>Privacy Policy | Terms & Conditions</Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default Footer;
