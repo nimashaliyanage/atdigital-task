2ac179e7015c2ebd4858a1be8cb7450c53d47330
nimashaliyanage <150325936+nimashaliyanage@users.noreply.github.com>
2024-04-06 11:45:08 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../Assets/ATD logo.png";
+import "../Components/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+}
